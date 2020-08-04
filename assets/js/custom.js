16dfeb363dab9a65a53a2c866dc05516b4a25a17
KrowTV <abrahamderese98@gmail.com>
2020-08-04 16:53:01 -0400

    remove

--- assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-var videoPlayer = document.getElementById("video-player");
-
-function setVideo(url){
-    videoPlayer.src = url;
-}
-
-function toggleDropdown(elem) {
-    elem.parentNode.childNodes[3].classList.toggle("show");
-}
